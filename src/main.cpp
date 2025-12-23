7f0dc647aaeb4884afa57dd53d70dc9faf0f2365
viomugle <2451051674@qq.com>
2025-12-23 10:18:35 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include <cstddef>
 #include <cstdint>
 #include <execution>
+#include <fstream>
+#include <ios>
+#include <iterator>
 #include <limits>
 #include <pthread.h>
 #include <string_view>
+#include <unistd.h>
+#include <utility>
 #include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -16,6 +21,7 @@
 #include <set>
 #include <stdexcept>
 #include <vector>
+
 #define ptr(A) &A
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -117,8 +123,8 @@ private:
     createLogicalDevice();
     createSwapChain();
     createImageViews();
+    createGraphicsPipeline();
   }
-
 
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
@@ -128,7 +134,10 @@ private:
 
   void cleanup() {
 
-    std::for_each(std::execution::par)
+    std::for_each(std::execution::par, swapChainImageViews.begin(),
+                  swapChainImageViews.end(), [this](VkImageView imageView) {
+                    vkDestroyImageView(this->device, imageView, nullptr);
+                  });
 
     vkDestroySwapchainKHR(device, swapChain, nullptr);
     vkDestroyDevice(device, nullptr);
@@ -495,43 +504,76 @@ private:
                              ptr(swapChain))) {
       throw std::runtime_error("falied to create swap chain");
     }
-    vkGetSwapchainImagesKHR(device,swapChain,ptr(imageCount),nullptr);
+    vkGetSwapchainImagesKHR(device, swapChain, ptr(imageCount), nullptr);
     swapChainImages.resize(imageCount);
-    vkGetSwapchainImagesKHR(device,swapChain,ptr(imageCount),swapChainImages.data());\
-    swapChainImageFormat=surfaceFormat.format;
-    swapChainExtent=extent;
+    vkGetSwapchainImagesKHR(device, swapChain, ptr(imageCount),
+                            swapChainImages.data());
+    swapChainImageFormat = surfaceFormat.format;
+    swapChainExtent = extent;
   }
 
-  void createImageViews()
-  {
-      swapChainImageViews.resize(swapChainImageViews.size());
+  void createImageViews() {
+    swapChainImageViews.resize(swapChainImageViews.size());
 
-      for(size_t i=0;i<swapChainImageViews.size();i++)
-      {
-          VkImageViewCreateInfo createInfo{};
-          createInfo.sType=VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-          createInfo.image=swapChainImages[i];
-          createInfo.viewType=VK_IMAGE_VIEW_TYPE_2D;
-          createInfo.format=swapChainImageFormat;
-          createInfo.components.r=VK_COMPONENT_SWIZZLE_IDENTITY;
-          createInfo.components.g=VK_COMPONENT_SWIZZLE_IDENTITY;
-          createInfo.components.b=VK_COMPONENT_SWIZZLE_IDENTITY;
-          createInfo.components.a=VK_COMPONENT_SWIZZLE_IDENTITY;
-          createInfo.subresourceRange.aspectMask=VK_IMAGE_ASPECT_COLOR_BIT;
-          createInfo.subresourceRange.baseMipLevel=0;
-          createInfo.subresourceRange.levelCount=1;
-          createInfo.subresourceRange.baseArrayLayer=0;
-          createInfo.subresourceRange.layerCount=1;
-          if(vkCreateImageView(device,ptr(createInfo),nullptr,&swapChainImageViews[i]))
-          {
-              throw std::runtime_error("failed to create image views");
-          }
-
+    for (size_t i = 0; i < swapChainImageViews.size(); i++) {
+      VkImageViewCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+      createInfo.image = swapChainImages[i];
+      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+      createInfo.format = swapChainImageFormat;
+      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+      createInfo.subresourceRange.baseMipLevel = 0;
+      createInfo.subresourceRange.levelCount = 1;
+      createInfo.subresourceRange.baseArrayLayer = 0;
+      createInfo.subresourceRange.layerCount = 1;
+      if (vkCreateImageView(device, ptr(createInfo), nullptr,
+                            &swapChainImageViews[i])) {
+        throw std::runtime_error("failed to create image views");
       }
+    }
+  }
 
+  VkShaderModule createShaderModule(const std::vector<char>& code)
+  {
+      VkShaderModuleCreateInfo createInfo{};
+      createInfo.sType=VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+      createInfo.codeSize=code.size();
+      createInfo.pCode=(uint32_t)code.data();
+      VkShaderModule shaderModule;
+      if(vkCreateShaderModule(device,ptr(createInfo),nullptr,ptr(shaderModule))!=VK_SUCCESS)
+          throw std::runtime_error("failed to create shader module");
+      return shaderModule;
+  }
+
+  void createGraphicsPipeline(){
+      auto vertShaderCode=readFile("shaders/vert.spv");
+      auto fragShaderCode=readFile("shaders/frag.spv");
+      VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+      vertShaderStageInfo.sType=VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+      vertShaderStageInfo.stage=VK_SHADER_STAGE_VERTEX_BIT;
+      vertShaderStageInfo.module=vertShaderModule;
+      vertShaderStageInfo.pName="main";
+      
+      
 
   }
 
+  static std::vector<char> readFile(const std::string &filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+    if (!file.is_open()) {
+      throw std::runtime_error("failed to open file!");
+    }
+    size_t fileSize = (size_t)file.tellg();
+    std::vector<char> buffer(fileSize);
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+    return buffer;
+  }
 };
 
 int main() {
