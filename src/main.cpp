78840e9cba810a2991f375ca7e5b5d081b680923
viomugle <2451051674@qq.com>
2025-12-23 13:39:35 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <concepts>
 #include <cstddef>
 #include <cstdint>
 #include <execution>
@@ -25,6 +26,11 @@
 #define ptr(A) &A
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+std:: vector<VkDynamicState> dynamicStates={
+    VK_DYNAMIC_STATE_VIEWPORT,
+    VK_DYNAMIC_STATE_SCISSOR
+};
 
 struct SwapChainSupportDetails {
   VkSurfaceCapabilitiesKHR capabilities;
@@ -542,7 +548,7 @@ private:
       VkShaderModuleCreateInfo createInfo{};
       createInfo.sType=VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
       createInfo.codeSize=code.size();
-      createInfo.pCode=(uint32_t)code.data();
+      createInfo.pCode=(uint32_t *)code.data();
       VkShaderModule shaderModule;
       if(vkCreateShaderModule(device,ptr(createInfo),nullptr,ptr(shaderModule))!=VK_SUCCESS)
           throw std::runtime_error("failed to create shader module");
@@ -550,15 +556,92 @@ private:
   }
 
   void createGraphicsPipeline(){
-      auto vertShaderCode=readFile("shaders/vert.spv");
-      auto fragShaderCode=readFile("shaders/frag.spv");
+      auto vertShaderCode = readFile("shaders/vert.spv");
+      auto fragShaderCode = readFile("shaders/frag.spv");
+      VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+      VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
       VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
       vertShaderStageInfo.sType=VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
       vertShaderStageInfo.stage=VK_SHADER_STAGE_VERTEX_BIT;
       vertShaderStageInfo.module=vertShaderModule;
       vertShaderStageInfo.pName="main";
+      VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+      fragShaderStageInfo.sType=VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+      fragShaderStageInfo.stage=VK_SHADER_STAGE_FRAGMENT_BIT;
+      fragShaderStageInfo.module=fragShaderModule;
+      fragShaderStageInfo.pName="main";
+      VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+      VkPipelineDynamicStateCreateInfo dynamicState{};
+      dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+      dynamicState.dynamicStateCount = dynamicStates.size();
+      dynamicState.pDynamicStates = dynamicStates.data();
+
+      VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+      vertexInputInfo.sType=VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+      vertexInputInfo.vertexBindingDescriptionCount=0;
+      vertexInputInfo.pVertexBindingDescriptions=nullptr;
+      vertexInputInfo.vertexAttributeDescriptionCount=0;
+      vertexInputInfo.pVertexAttributeDescriptions=nullptr;
+
+      VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
+      inputAssembly.sType=VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+      inputAssembly.topology=VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+      inputAssembly.primitiveRestartEnable=VK_FALSE;
+
+      VkViewport viewport{};
+      viewport.x=0.0f;
+      viewport.y=0.0f;
+      viewport.width=swapChainExtent.width;
+      viewport.height=swapChainExtent.height;
+      viewport.minDepth=0.0f;
+      viewport.maxDepth=0.0f;
+      VkRect2D scissor{};
+      scissor.offset={0,0};
+      scissor.extent=swapChainExtent;
+
+      VkPipelineViewportStateCreateInfo viewportState{};
+      viewportState.sType=VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+      viewportState.viewportCount=1;
+      viewportState.scissorCount=1;
+      viewportState.pViewports=ptr(viewport);
+      viewportState.pScissors=ptr(scissor);
+
+      VkPipelineRasterizationStateCreateInfo rasterizer{};
+      rasterizer.sType=VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+      rasterizer.depthBiasClamp=VK_FALSE;
+      rasterizer.rasterizerDiscardEnable=VK_FALSE;
+      rasterizer.polygonMode=VK_POLYGON_MODE_FILL;
+      rasterizer.lineWidth=1.0f;
+      rasterizer.cullMode=VK_CULL_MODE_BACK_BIT;
+      rasterizer.frontFace=VK_FRONT_FACE_CLOCKWISE;
+
+      rasterizer.depthBiasEnable=VK_FALSE;
+      rasterizer.depthBiasConstantFactor=0.0f;
+      rasterizer.depthBiasClamp=0.0f;
+      rasterizer.depthBiasSlopeFactor=0.0f;
+
+      VkPipelineMultisampleStateCreateInfo multisampling{};
+      multisampling.sType=VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+      multisampling.sampleShadingEnable=VK_FALSE;
+      multisampling.rasterizationSamples=VK_SAMPLE_COUNT_1_BIT;
+      multisampling.minSampleShading=1.0f;
+      multisampling.pSampleMask=nullptr;
+      multisampling.alphaToCoverageEnable=VK_FALSE;
+      multisampling.alphaToOneEnable=VK_FALSE;
+
+      VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+      colorBlendAttachment.colorWriteMask=VK_COLOR_COMPONENT_R_BIT|VK_COLOR_COMPONENT_G_BIT|VK_COLOR_COMPONENT_B_BIT|VK_COLOR_COMPONENT_A_BIT;
+      colorBlendAttachment.blendEnable=VK_FALSE;
+      colorBlendAttachment.srcColorBlendFactor=VK_BLEND_FACTOR_ONE;
+      colorBlendAttachment.dstColorBlendFactor=VK_BLEND_FACTOR_ZERO;
+      colorBlendAttachment.colorBlendOp=VK_BLEND_OP_ADD;
+      colorBlendAttachment.srcAlphaBlendFactor=VK_BLEND_FACTOR_ONE;
+      colorBlendAttachment.dstAlphaBlendFactor=VK_BLEND_FACTOR_ZERO;
       
+      VkPipelineColorBlendStateCreateInfo colorBlending{};
       
+
+
 
   }
 
